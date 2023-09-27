4e4b7b0604dbe961c223c304e8185f8728d75fb3
VitthalGund <vitthalgund30@gmail.com>
2023-09-28 00:08:00 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const mongodb = require("valid-url");
+const valid = require("valid-url");
 const mongoose = require("mongoose");
 const bp = require("body-parser");
-const shortID = require("short-unique-id")
+const sid = require("shortid");
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 mongoose.connect(process.env.URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -27,6 +27,9 @@ const urlSchema = new Schema({
   short_url: String
 });
 const Urls = mongoose.model("URLs", urlSchema);
+const Counter = mongoose.model("Counter", new Schema({
+  count: Number
+}));
 
 app.use(cors());
 
@@ -41,23 +44,27 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/shorturl/:short', function (req, res) {
+  Urls.find({ short_url: req.params.short }).then(function (docs) {
+    res.redirect(docs[0].original_url);
+  });
+});
 
-app.post("/api/short/new", function (req, res) {
-  const url = mongodb.isWebUri(req.body.url);
+app.post("/api/shorturl", async function (req, res) {
+  const url = valid.isWebUri(req.body.url);
   if (url != undefined) {
-    let id = new shortID({ length: 2 });
+    let id = sid.generate();
 
     let newUrl = new Urls({
       original_url: url,
       short_url: id,
     });
-    newUrl.save(function (err, doc) {
-      if (err) return console.error(err);
-      res.json({
-        original_url: newUrl.original_url,
-        short_url: newUrl.short_url
-      });
+    const update = await newUrl.save();
+    res.json({
+      original_url: newUrl.original_url,
+      short_url: newUrl.short_url
     });
+
   }
   else {
     res.json({ "error": "invalid URL" });
